05f71721368752767c79db06b71ca627413fbc04
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-08-17 11:04:45 +0900

    Update bib2xml.py (Add logging etc.)

--- bib2xml.py
+++ bib2xml.py
@@ -3,6 +3,7 @@
 import argparse
 import sys
 import xml.etree.cElementTree as ET
+from logging import DEBUG
 from pathlib import Path
 from typing import Optional, Union
 
@@ -14,12 +15,16 @@ else:
 from pybtex.database.input import bibtex  # https://github.com/chbrown/pybtex
 
 from bib2xml.helper import convert
+from bib2xml.logging import get_child_logger
+
+_logger = get_child_logger(__name__)
 
 cli_parser = argparse.ArgumentParser()
 cli_parser.add_argument(
     "-a",
     "--append",
     dest="inxml",
+    type=Path,
     help="existing filename (e.g. Sources.xml) to append elements to",
 )
 cli_parser.add_argument(
@@ -34,6 +39,7 @@ cli_parser.add_argument(
     "-i",
     "--input",
     dest="bibtexfile",
+    required=True,
     type=Path,
     help="input bibtex filename",
 )
@@ -48,24 +54,20 @@ cli_parser.add_argument(
 )
 args = cli_parser.parse_args()
 
+inxml: Optional[Path] = args.inxml
+bibtexfile: Path = args.bibtexfile
+xmlfile: Optional[Union[Path, Literal[True]]] = args.xmlfile
+
 bib_parser = bibtex.Parser()
 
-try:
-    bibdata = bib_parser.parse_file(args.bibtexfile)
-except NameError:
-    print("Need an input filename. See --help", file=sys.stderr)
-    sys.exit(1)
 
-if len(args) > 0:
-    print(
-        "Warning: extra arguments ignored: " % " ".join(args), file=sys.stderr
-    )
+bibdata = bib_parser.parse_file(bibtexfile)
 
 url_schema = "http://schemas.microsoft.com/office/word/2004/10/bibliography"
 try:
     ET.register_namespace("", url_schema)
     ET.register_namespace("b", url_schema)
-    root = ET.parse(args.inxml).getroot()
+    root = ET.parse(inxml).getroot()
 except TypeError:
     root = ET.Element(
         "b:Sources",
@@ -74,7 +76,8 @@ except TypeError:
 
 for key, entry in bibdata.entries.items():
     if args.debug:
-        print(key)
+        _logger.setLevel(DEBUG)
+        _logger.debug(key)
     source = ET.SubElement(root, "b:Source")
     tag = ET.SubElement(source, "b:Tag")
     tag.text = key
@@ -147,7 +150,7 @@ output2 = convert(output)
 # tree = ET.ElementTree(xml_file)
 # tree.write("xml_output.xml")
 try:
-    with open(args.xmlfile, "wb") as f:
+    with open(xmlfile, mode="wb") as f:
         f.write(output2.encode("utf-8")[2:-1])
 except TypeError:
     print(output2)
