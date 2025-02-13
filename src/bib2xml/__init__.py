8139a30870e4bd01d37ec28634bec7b20d481033
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-02-13 20:47:57 +0900

    docs: Add docstring

--- src/bib2xml/__init__.py
+++ src/bib2xml/__init__.py
@@ -1,8 +1,35 @@
-"""My own python package's template."""
+"""Processes Bibtex files (.bib), produces Word Bibliography XML (.xml) output
+
+Example
+-------
+
+bash
+^^^^
+
+.. code-block:: bash
+
+    bib2xml -i bib-example.bib -o bib-example-xml.xml
+
+Python
+^^^^^^
+
+.. code-block:: python
+
+    from bib2xml import bib2xml
+    from pybtex.database.input import bibtex
+
+    bib_parser = bibtex.Parser()
+    xml_str = bib2xml(bib_parser.parse_file("bib-example.bib"))
+    with open("bib-example-xml.xml", mode="w") as f:
+        f.write(xml_str)
+
+"""
+
+from bib2xml._core._core import bib2xml
 
 __version__ = "0.1.4"
 __license__ = "MIT"
 __author__ = "yu9824"
 __copyright__ = "Copyright © 2024 yu9824"
 
-# __all__ = ()
+__all__ = ("bib2xml",)
